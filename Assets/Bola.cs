3346bcaa761d1fd8b623ffa12047de898dc76f0c
Julia Nicola <julia.nicola@hotmail.com>
2019-08-28 18:00:25 -0300

    estrutura do jogo

---
+++ Assets/Bola.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bola : MonoBehaviour {
+
+    float velTrans1;
+    Vector2 direcao;
+
+	void Start () {
+        velTrans1 = 0.1f;
+        direcao = -Vector2.one;
+	}
+
+	void Update () {
+        this.transform.Translate(direcao * velTrans1);
+        if(this.transform.position.y + this.transform.localScale.y/2 > Gerenciador.cantoSupDir.y){
+			this.direcao.y = -this.direcao.y;	
+		}
+		if(this.transform.position.y + this.transform.localScale.y/2 <= Gerenciador.cantoInfEsq.y){
+			this.direcao.y = -this.direcao.y;	
+		}
+
+	}
+	
+	private void OnTriggerEnter2D(Collider2D collision){
+		if(collision.tag.Equals("RaqEsq") || collision.tag.Equals("RaqDir")){
+			Debug.Log("TESTE");
+			this.direcao.x = -this.direcao.x;
+		}
+			
+	}
+}
---
+++ Assets/Gerenciador.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gerenciador : MonoBehaviour {
+
+    public static Vector2 cantoInfEsq;
+    public static Vector2 cantoSupDir;
+    float velTrans1;
+    public GameObject RaqEsq;
+    public GameObject RaqDir;
+
+    void Start () {
+        velTrans1 = 0.2f;
+        cantoInfEsq = Camera.main.ScreenToWorldPoint(new Vector2(0,0));
+        cantoSupDir = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width-1,Screen.height-1));
+	}
+
+    void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveParaCima(RaqEsq);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveParaBaixo(RaqEsq);
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            moveParaCima(RaqDir);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            moveParaBaixo(RaqDir);
+        }
+
+
+	}
+
+    void moveParaCima(GameObject g)
+    {
+        if(g.transform.position.y + g.transform.localScale.y/2 < cantoSupDir.y)
+             g.transform.Translate(0, velTrans1, 0);
+    }
+     void moveParaBaixo(GameObject g)
+    {
+        if (g.transform.position.y + g.transform.localScale.y / 2 > cantoInfEsq.y)
+            g.transform.Translate(0, -velTrans1, 0);
+    }
+
+}
